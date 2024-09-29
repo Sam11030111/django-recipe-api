e994cebb1fc2fa345034f5c0aa7c9196fcbf5eb6
Sam11030111 <sam86110306@gmail.com>
2024-09-29 13:47:08 -0500

    Build Ingredients API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -72,9 +72,19 @@ class ModelTests(TestCase):
 
         self.assertEqual(str(recipe), recipe.title)
 
-    def test_crate_tag(self):
+    def test_create_tag(self):
         """Test creating a tag is successful."""
         user = create_user()
         tag = models.Tag.objects.create(user=user, name='Tag1')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_indegrient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1',
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
